6266a3e05552eade787e1d55cde5d2f837d7f00b
Wyatt <Wyatt.endres@gmail.com>
2017-07-23 12:50:28 -0400

    pom.xml

---
+++ spec-db/src/main/java/com/speculation1000/specdb/spec_db/App.java
@@ -0,0 +1,13 @@
+package com.speculation1000.specdb.spec_db;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
