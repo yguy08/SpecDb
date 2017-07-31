4afc80a0f8cc17799a74a5ebd813856baa6cc492
Wyatt Endres <wyatt.endres@gmail.com>
2017-07-31 17:14:23 -0400

    Update PracticeCommands.sql

--- src/main/java/utils/PracticeCommands.sql
+++ src/main/java/utils/PracticeCommands.sql
@@ -1,3 +1,9 @@
 WITH TrueRange AS (
 SELECT t1.*, MAX(Round(t1.high - t1.low,8),Abs(Round(t1.high - t2.low,8)),Abs(Round(t1.low - t2.close,8))) AS TrueRange from markets t1,markets t2 where t1.Date = t2.Date + 86400 AND t1.Symbol = t2.Symbol order by date DESC)
-SELECT * FROM TrueRange
+SELECT * FROM TrueRange;
+
+WITH C_Close AS (
+SELECT Symbol, Date AS C_Date, Close AS C_Close from markets where date = (SELECT MAX(Date) from markets where Symbol = 'STEEMBTC') AND Symbol = 'STEEMBTC'),
+High AS (Select C_Date - Date AS High from markets,C_Close where Close > (SELECT C_Close from C_Close) AND markets.Symbol = 'STEEMBTC' order by date DESC limit 1),
+Low AS (Select C_Date - Date AS Low from markets,C_Close where Close < (SELECT C_Close from C_Close) AND markets.Symbol = 'STEEMBTC' order by date DESC limit 1)
+SELECT Max(High,Low) from High, Low;
