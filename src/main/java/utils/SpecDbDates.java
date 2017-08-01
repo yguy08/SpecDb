5af0793408f52c3b3802a583c8906560788da2f1
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-01 17:10:46 -0400

    Update SpecDbDates.java

--- src/main/java/utils/SpecDbDates.java
+++ src/main/java/utils/SpecDbDates.java
@@ -9,18 +9,11 @@ import java.util.GregorianCalendar;
 
 public class SpecDbDates {
 	
-	public static long simpleDateToUtcMidnightSeconds(String yyyy_MM_dd){
-		String[] dateSplit = yyyy_MM_dd.split("-");
-		int year = Integer.parseInt(dateSplit[0]);
-		int month = Integer.parseInt(dateSplit[1]);
-		int day = Integer.parseInt(dateSplit[2]);
-		LocalDateTime closeTime = LocalDateTime.of(year, month, day, 0, 0, 0);
-		ZoneId closeZone = ZoneId.of("Etc/UTC"); 
-		ZonedDateTime utcMidnight = ZonedDateTime.of(closeTime, closeZone).plusDays(1);
-		return utcMidnight.toEpochSecond();
-	}
-	
 	public static long dateToUtcMidnightSeconds(Date date){
+		String dateStr = Long.toString(date.getTime());
+		if(dateStr.length() < 11){
+			date = new Date(Long.parseLong(date.getTime() + "000"));
+		}
 		Calendar calendar = new GregorianCalendar();
 		calendar.setTime(date);
 		int year = calendar.get(Calendar.YEAR);
@@ -33,7 +26,6 @@ public class SpecDbDates {
 	}
 	
 	public static void main(String[]args){
-		System.out.println(simpleDateToUtcMidnightSeconds("2017-07-25"));
 		System.out.println(dateToUtcMidnightSeconds(new Date()));
 	}
 
