56ff693e7a8561a0e204ebc412baf8a49ff0d04c
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-01 17:08:32 -0400

    Update DbLoader.java

--- src/main/java/loader/DbLoader.java
+++ src/main/java/loader/DbLoader.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import utils.CreateMarketTable;
+import db.DbManager;
 
 public class DbLoader {
 	
@@ -18,7 +18,18 @@ public class DbLoader {
 		
 	public DbLoader() {
 		//create table if does not exist
-		new CreateMarketTable().insertIfNotExists();
+        String sql = "CREATE TABLE IF NOT EXISTS markets (\n"
+                + "	Symbol character NOT NULL,\n"
+                + "	Exchange character NOT NULL,\n"
+                + "	Date int NOT NULL,\n"
+                + " High decimal,\n"
+                + " Low decimal,\n"
+                + " Open decimal,\n"
+                + " Close decimal,\n"
+                + " Volume int,\n"
+                + " ATR decimal\n"
+                + ");";
+		new DbManager().createTable(sql);
 		
 		testNetworkConnection();
 		
