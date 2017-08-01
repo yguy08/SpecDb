d201d5135d8f2f891c47e5258a91813aff527648
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-01 17:08:02 -0400

    Update PracticeCommands.sql

--- src/main/java/utils/PracticeCommands.sql
+++ src/main/java/utils/PracticeCommands.sql
@@ -7,3 +7,32 @@ SELECT Symbol, Date AS C_Date, Close AS C_Close from markets where date = (SELEC
 High AS (Select C_Date - Date AS High from markets,C_Close where Close > (SELECT C_Close from C_Close) AND markets.Symbol = 'STEEMBTC' order by date DESC limit 1),
 Low AS (Select C_Date - Date AS Low from markets,C_Close where Close < (SELECT C_Close from C_Close) AND markets.Symbol = 'STEEMBTC' order by date DESC limit 1)
 SELECT Max(High,Low) from High, Low;
+
+SELECT m1.Symbol,m1.Date,m1.close,m1.close-m2.close
+FROM
+	Markets m1, Markets m2,
+	(SELECT t1.Symbol,t2.Date AS day1, MAX(t1.Date) AS day2
+	FROM Markets t1, Markets t2
+	WHERE t1.Date < t2.Date
+	AND t1.Symbol = t2.Symbol
+	GROUP BY t2.Date) AS prev
+WHERE m1.Date=prev.day1
+AND m2.Date=prev.day2
+AND m1.Symbol = m2.Symbol
+
+WITH cte AS
+(
+  SELECT 
+    t.[Time]
+   ,[H-L]    = Round([BID-HIGH]-[BID-LOW],5) 
+   ,[H-Cp]   = Abs(Round([BID-HIGH]-[prev_BID-CLOSE],5)) 
+   ,[L-Cp]   = Abs(Round([BID-LOW]-[prev_BID-CLOSE],5))
+  FROM (SELECT *, 
+               [prev_BID-CLOSE]  = LAG([BID-CLOSE]) OVER(ORDER BY [Time])
+        FROM [#tbl_GBP-USD_1-Day]) AS t
+)
+SELECT *
+FROM cte
+CROSS APPLY (SELECT MAX(v) AS v
+             FROM ( VALUES ([H-L]),([H-Cp]),([L-Cp])) AS value(v)
+) AS sub([TR]);
