bf6e3b3b047c3c1d2beaf0e01ca27893fcaadb9e
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-02 18:12:41 -0400

    Update CreateMarketTable.java

--- src/main/java/utils/CreateMarketTable.java
+++ src/main/java/utils/CreateMarketTable.java
@@ -4,20 +4,8 @@ import db.DbManager;
 
 public class CreateMarketTable {
 	
-	public static void main(String[] args){
-        String sql = "CREATE TABLE IF NOT EXISTS markets (\n"
-                + "	Symbol character NOT NULL,\n"
-                + "	Exchange character NOT NULL,\n"
-                + "	Date int NOT NULL,\n"
-                + " High decimal,\n"
-                + " Low decimal,\n"
-                + " Open decimal,\n"
-                + " Close decimal,\n"
-                + " Volume int,\n"
-                + " ATR decimal\n"
-                + ");";
-        
-    	new DbManager().createTable(sql);
+	public static void main(String[] args){        
+    	new DbManager().createTable();
 	}
 
 }
