cae4cf7048306e00bef9f75f073c26afb49ad7e2
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-02 18:12:01 -0400

    Update DbLoader.java

--- src/main/java/loader/DbLoader.java
+++ src/main/java/loader/DbLoader.java
@@ -1,46 +1,29 @@
 package loader;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import db.DbManager;
+import utils.SpecDbDate;
 
-public class DbLoader {
+public class DbLoader implements Runnable {
+	
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 	
 	private static boolean netConnected;
-		
-	public DbLoader() {
-		//create table if does not exist
-        String sql = "CREATE TABLE IF NOT EXISTS markets (\n"
-                + "	Symbol character NOT NULL,\n"
-                + "	Exchange character NOT NULL,\n"
-                + "	Date int NOT NULL,\n"
-                + " High decimal,\n"
-                + " Low decimal,\n"
-                + " Open decimal,\n"
-                + " Close decimal,\n"
-                + " Volume int,\n"
-                + " ATR decimal\n"
-                + ");";
-		new DbManager().createTable(sql);
-		
-		testNetworkConnection();
-		
-		if(isConnected()){			
-			//polo loader
-			new PoloniexLoader();
-		}else{
-			System.out.println("No network connection!");
-			System.out.println("Connect to a network and try again to get latest market updates.");
-		}
-	}
 	
 	private void testNetworkConnection() {
 		final Runnable stuffToDo = new Thread() {
@@ -78,7 +61,30 @@ public class DbLoader {
 	}
 	
 	public static void main(String[] args) {
-		new DbLoader();
+	    new DbLoader().updateEveryHour();
+	}
+	
+	public void updateEveryHour(){
+		long initialDelay = SpecDbDate.nextHourInitialDelay();
+		System.out.println("Starting updates in: " + initialDelay / 60 + " Minutes");
+	    scheduler.scheduleAtFixedRate(new DbLoader(), initialDelay, 60 * 60, SECONDS);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Start: " + ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Etc/UTC")));
+		new DbManager().createTable();
+		
+		testNetworkConnection();
+		
+		if(isConnected()){			
+			//polo loader
+			new PoloniexLoader();
+		}else{
+			System.out.println("No network connection!");
+			System.out.println("Connect to a network and try again to get latest market updates.");
+		}
+		System.out.println("End: " + ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Etc/UTC")));
 	}
 	
 }
