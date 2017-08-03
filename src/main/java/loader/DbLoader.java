0590a9487bf5a8cd2245f79f1975403d31cef168
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-03 17:41:37 -0400

    Update DbLoader.java

--- src/main/java/loader/DbLoader.java
+++ src/main/java/loader/DbLoader.java
@@ -18,12 +18,66 @@ import java.util.concurrent.TimeoutException;
 
 import db.DbManager;
 import utils.SpecDbDate;
+import utils.log.SpecDbLogger;
 
 public class DbLoader implements Runnable {
 	
 	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 	
 	private static boolean netConnected;
+	
+	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
+	
+	public static boolean isConnected() {
+		return netConnected;
+	}
+	
+	public static void main(String[] args) {
+		specLogger.log(DbLoader.class.getName(), "Start up");
+	    new DbLoader().updateEveryHour();
+	}
+	
+	public void updateEveryHour(){
+		long initialDelay = SpecDbDate.nextHourInitialDelay();
+		scheduler.scheduleAtFixedRate(new DbLoader(), initialDelay, 60 * 60, SECONDS);
+		specLogger.log(DbLoader.class.getName(), "Starting updates in " + initialDelay / 60);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("Start: " + ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Etc/UTC")));
+		new DbManager().createTable();
+		
+		testNetworkConnection();
+		
+		new PoloniexLoader();
+		
+		if(isConnected()){			
+			try{
+				new PoloniexLoader();
+			}catch(Exception e){
+				specLogger.log(DbLoader.class.getName(), "Poloniex failed...Trying Bittrex");
+			}
+			
+			try{
+				new BittrexLoader();
+			}catch(Exception e){
+				System.out.println("Bittrex failed..");
+				specLogger.log(DbLoader.class.getName(), "Poloniex failed...Trying Bittrex");
+			}
+			
+			System.out.println("Done...");
+			specLogger.log(DbLoader.class.getName(), "Poloniex failed...Trying Bittrex");
+			
+		}else{
+			specLogger.log(DbLoader.class.getName(), "No network connection!");
+			specLogger.log(DbLoader.class.getName(), "Connect to a network and try again to get latest market updates.");
+		}
+		
+		System.out.println("End: " + ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Etc/UTC")));
+		long initialDelay = SpecDbDate.nextHourInitialDelay();
+		specLogger.log(DbLoader.class.getName(), "Next update: " + initialDelay / 60 + " Minutes");
+	}
 	
 	private void testNetworkConnection() {
 		final Runnable stuffToDo = new Thread() {
@@ -54,49 +108,6 @@ public class DbLoader implements Runnable {
 			if (!executor.isTerminated()){
 				executor.shutdownNow();
 			}
-	}
-	
-	public static boolean isConnected() {
-		return netConnected;
-	}
-	
-	public static void main(String[] args) {
-	    new DbLoader().updateEveryHour();
-	}
-	
-	public void updateEveryHour(){
-		long initialDelay = SpecDbDate.nextHourInitialDelay();
-		System.out.println("Starting updates in: " + initialDelay / 60 + " Minutes");
-	    scheduler.scheduleAtFixedRate(new DbLoader(), 0, 60 * 60, SECONDS);
-	}
-
-	@Override
-	public void run() {
-		System.out.println("Start: " + ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Etc/UTC")));
-		new DbManager().createTable();
-		
-		testNetworkConnection();
-		
-		if(isConnected()){			
-			//polo loader
-			try{
-				new PoloniexLoader();
-			}catch(Exception e){
-				System.out.println("Poloniex failed...Trying Bittrex");
-			}
-			
-			try{
-				new BittrexLoader();
-			}catch(Exception e){
-				System.out.println("Bittrex failed..");
-			}
-			
-			System.out.println("Done...");
-		}else{
-			System.out.println("No network connection!");
-			System.out.println("Connect to a network and try again to get latest market updates.");
-		}
-		System.out.println("End: " + ZonedDateTime.of(LocalDateTime.now(), ZoneId.of("Etc/UTC")));
 	}
 	
 }
