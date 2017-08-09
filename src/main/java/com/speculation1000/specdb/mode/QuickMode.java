b93c49dd408062cfbb3958be1ad905e97ad167f0
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-09 17:57:18 -0400

    Update QuickMode.java

--- src/main/java/com/speculation1000/specdb/mode/QuickMode.java
+++ src/main/java/com/speculation1000/specdb/mode/QuickMode.java
@@ -1,104 +1,60 @@
-package com.speculation1000.specdb.mode;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
+package com.speculation1000.specdb.start;
 
 import java.time.Instant;
-import java.util.StringJoiner;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.logging.Level;
 
-import com.speculation1000.specdb.dao.BittrexDAO;
-import com.speculation1000.specdb.dao.MarketSummaryDAO;
-import com.speculation1000.specdb.dao.PoloniexDAO;
+import com.speculation1000.specdb.db.DbUtils;
 import com.speculation1000.specdb.log.SpecDbLogger;
-import com.speculation1000.specdb.market.Market;
-import com.speculation1000.specdb.start.SpecDbDate;
-import com.speculation1000.specdb.start.SpecDbException;
-import com.speculation1000.specdb.start.SpecDbTime;
-import com.speculation1000.specdb.start.StartRun;
+import com.speculation1000.specdb.mode.Mode;
+import com.speculation1000.specdb.mode.ModeFactory;
+import com.speculation1000.specdb.mode.QuickMode;
 
-public class QuickMode implements Mode {
-	
-	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+public class StartApp {
 	
 	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
 	
-	private static final int PERIOD = 60 * 15;
+	private static final Instant START_UP_TS = Instant.now();
+	
+	public StartApp(){
+		specLogger.logp(Level.INFO, StartApp.class.getName(), "main", StartApp.startUpStatusMessage());
+		new DbUtils().createTable();
+	}
 
-	@Override
-	public String getStartRunMessage() {
+	public static void main(String[] args) {
+		new StartApp();
+		if(args.length > 0){
+			Mode mode = ModeFactory.getMode(args[0]);
+			mode.startApp();
+		}else{
+			new QuickMode().startApp();
+		}
+	}
+
+	public static Instant getStartUpTs() {
+		return START_UP_TS;
+	}
+	
+	/**
+	 * @returns seconds of system up time
+	 */
+	public static long getSystemUptime(){
+		return Instant.now().getEpochSecond() - StartApp.getStartUpTs().getEpochSecond();
+	}
+	
+	public static String startUpStatusMessage(){
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n");
+		sb.append("********** SpecDb ************** \n");
+		sb.append("[ Digital Asset Price Database ]\n");
 		sb.append("********************************\n");
-		sb.append("          [ QUICKMODE ]\n");
-		sb.append("********************************\n");
-		sb.append("            [ START ]\n");
+		sb.append("* Start Up: \n");
 		sb.append("* At: ");
-		sb.append(SpecDbDate.instantToLogStringFormat(StartRun.getStartRunTS()) + "\n");
-		sb.append("* New Day: " + SpecDbDate.isNewDay(StartRun.getStartRunTS()) + "\n");
+		sb.append(SpecDbDate.instantToLogStringFormat(getStartUpTs()) + "\n");
 		sb.append("********************************\n");
+		long nextUpdate = SpecDbTime.getQuickModeDelaySeconds(Instant.now());
+		sb.append("* Next Update in " + nextUpdate + " seconds\n");
+		sb.append("********************************\n");		
 		return sb.toString();
 	}
-
-	@Override
-	public String getEndRunMessage() {
-		StringBuilder sb = new StringBuilder();
-		StringJoiner sj = new StringJoiner(":", "[", "]");
-		sb.append("\n");
-		sb.append("********************************\n");
-		sb.append("          [ QUICKMODE ]\n");
-		Instant end = Instant.now();
-		sb.append("********************************\n");
-		sb.append("          [ RESULTS ]\n");
-		sb.append("* Time: ");
-		sb.append(end.getEpochSecond() - StartRun.getStartRunTS().getEpochSecond() + " sec\n");
-		sb.append("********************************\n");
-		for(Market market : MarketSummaryDAO.getLatest()){
-			sj.add(market.toString());
-		}
-		sb.append(sj.toString() + "\n");
-		sb.append("********************************\n");
-		int i = SpecDbTime.getQuickModeDelay(Instant.now());
-		if(i == 0){
-			i = PERIOD / 60;
-		}
-		sb.append("* Next Update in " + i + " minutes\n");
-		sb.append("********************************\n");
-		return sb.toString();
-	}
-
-	@Override
-	public void startApp() {
-		long nextQuarterInitialDelay = SpecDbTime.getQuickModeDelay(Instant.now());
-		scheduler.scheduleAtFixedRate(new QuickMode(), nextQuarterInitialDelay * 60, PERIOD, SECONDS);		
-	}
-
-	@Override
-	public void run() {
-		StartRun.setStartRunTS();
-		specLogger.log(QuickMode.class.getName(),getStartRunMessage());
-		
-		PoloniexDAO polo = new PoloniexDAO();
-		
-		try{
-			polo.updateMarkets();
-		}catch(com.speculation1000.specdb.start.SpecDbException e){
-			specLogger.log(QuickMode.class.getName(),e.getMessage());
-		}
-		
-		BittrexDAO bittrex = new BittrexDAO();
-		try{
-			bittrex.updateMarkets();
-		}catch(SpecDbException e){
-			specLogger.log(QuickMode.class.getName(),e.getMessage());
-		}
-		
-		if(SpecDbDate.isNewDay(StartRun.getStartRunTS())){
-			specLogger.log(QuickMode.class.getName(),"New Day!");
-		}
-		
-		specLogger.log(QuickMode.class.getName(),getEndRunMessage());
-	}
-
 
 }
