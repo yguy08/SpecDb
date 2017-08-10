b123b0613d0931df288890df47fc3c9ddfd8a9c3
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-10 17:01:17 -0400

    Update DbTest.java

--- src/test/java/com/speculation1000/specdb/db/DbTest.java
+++ src/test/java/com/speculation1000/specdb/db/DbTest.java
@@ -1,5 +1,7 @@
 package com.speculation1000.specdb.db;
 
+import static org.junit.Assert.*;
+
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,13 +14,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.speculation1000.specdb.market.Market;
-import com.speculation1000.specdb.start.SpecDbDate;
+import com.speculation1000.specdb.start.SpecDbException;
+import com.speculation1000.specdb.time.SpecDbDate;
 
 public class DbTest {
 	
 	public static Connection connection;
 	
-	public static String[] symbolArr = {"BTCUSDT","ETHBTC","ETCBTC","XMRBTC","LTCBTC","STEEMBTC","STRATBTC","DGBBTC"};
+	public static String[] symbolArr = {"BTCUSDT","ETHBTC","ETCBTC","XMRBTC","LTCBTC"};
 	
 	public static BigDecimal price = new BigDecimal(0.01);
 	
@@ -30,59 +33,97 @@ public class DbTest {
 	
 	public static Instant endInstant;
 	
+	public static long yesterday;
+	
+	public static long today;
+	
+	public static long tomorrow;
+	
 	@BeforeClass
 	public static void setUpDb() {
 	    connection = DbUtils.testConnect();
 	    DbUtils.dropTable(connection);
 	    DbUtils.createTable(connection);
-	    startInstant = SpecDbDate.getTodayMidnightInstant(Instant.now());
-	    Market market;
+	    
+	    //insert test data 1 batch of yesterday
+	    Market yesterdaysMarket;
+	    yesterday = SpecDbDate.getYesterdayEpochSeconds(Instant.now());
 	    for(String symbol : symbolArr){
-	    	market = new Market();
-			market.setSymbol(symbol);
-			market.setExchange("POLO");
-			market.setDate(startInstant.getEpochSecond());
-			market.setHigh(price);
-			market.setLow(price);
-			market.setOpen(price);
-			market.setClose(price);
-			market.setVolume(volume);
-			marketList.add(market);
-		    	
-			for(int i = 1;i < 98;i++){
-				endInstant = startInstant.plusSeconds(i*15*60 - (60));
-	    		market = new Market();
-				market.setSymbol(symbol);
-				market.setExchange("POLO");
-				market.setDate(endInstant.getEpochSecond());
-				market.setHigh(price);
-				market.setLow(price);
-				market.setOpen(price);
-				market.setClose(price);
-				market.setVolume(volume);
-				marketList.add(market);	    		
-		    }
+	    	yesterdaysMarket = new Market();
+	    	yesterdaysMarket.setSymbol(symbol);
+	    	yesterdaysMarket.setExchange("POLO");
+	    	yesterdaysMarket.setDate(yesterday);
+	    	yesterdaysMarket.setHigh(price);
+	    	yesterdaysMarket.setLow(price);
+	    	yesterdaysMarket.setOpen(price);
+	    	yesterdaysMarket.setClose(price);
+	    	yesterdaysMarket.setVolume(volume);
+			marketList.add(yesterdaysMarket);	    	
+	    }
+	    
+	    //insert test data from 8/10 0:14 - 8/10 23:59
+	    Market todaysMarket;
+	    today = SpecDbDate.getTodayUtcEpochSeconds(Instant.now()) + 14 * 60;
+	    for(String symbol : symbolArr){
+	    	for(int i = 0;i < 15 * 4 * 24;i+=15){
+	    		todaysMarket = new Market();
+	    		todaysMarket.setSymbol(symbol);
+	    		todaysMarket.setExchange("POLO");
+	    		todaysMarket.setDate(today + i*60);
+	    		todaysMarket.setHigh(price);
+	    		todaysMarket.setLow(price);
+	    		todaysMarket.setOpen(price);
+	    		todaysMarket.setClose(price);
+	    		todaysMarket.setVolume(volume);
+				marketList.add(todaysMarket);
+	    	}
+	    }
+	    
+	    //insert test data with 8/11 0:14
+	    Market tomorrowsMarket;
+	    tomorrow = SpecDbDate.getTodayUtcEpochSeconds(Instant.now()) + 86400 + 14;
+	    for(String symbol : symbolArr){
+	    	tomorrowsMarket = new Market();
+	    	tomorrowsMarket.setSymbol(symbol);
+	    	tomorrowsMarket.setExchange("POLO");
+	    	tomorrowsMarket.setDate(tomorrow);
+	    	tomorrowsMarket.setHigh(price);
+	    	tomorrowsMarket.setLow(price);
+	    	tomorrowsMarket.setOpen(price);
+	    	tomorrowsMarket.setClose(price);
+	    	tomorrowsMarket.setVolume(volume);
+			marketList.add(tomorrowsMarket);
 	    }
 	    
 	    DbUtils.insertBatchMarkets(connection, marketList);
 	    
-	    try {
-			connection.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	@Test
 	public void testNextDayCleanUp(){
-			
+		try {
+			//using tomorrow because dates altered for test purposes to be run at any time
+			DbUtils.nextDayCleanUp(connection,"POLO",yesterday,tomorrow);
+		} catch (SpecDbException e) {
+			e.printStackTrace();
+		}
+		
+		String sqlCommand = "SELECT * FROM Markets where date = " + yesterday;
+		List<Market> marketList = DbUtils.genericMarketQuery(connection, sqlCommand);
+		for(Market market : marketList){
+			assertEquals(yesterday,market.getDate());
+		}
 	}
 	
 	
 	
     @AfterClass 
     public static void logout() {
-    	
+	    try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
     }
 
 }
