bcec7d3c8cb75af62caa59952a4032c398c3f21a
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-10 17:02:43 -0400

    Add files via upload

---
+++ src/test/java/com/speculation1000/specdb/time/SpecDbTimeRunner.java
@@ -0,0 +1,20 @@
+package com.speculation1000.specdb.time;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class SpecDbTimeRunner {
+
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(SpecDbTimeTest.class);
+		
+		for(Failure failure : result.getFailures()){
+			System.out.println(failure.toString());
+		}
+		
+		System.out.println(result.wasSuccessful());
+
+	}
+
+}
