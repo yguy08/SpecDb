bff7df19d354ccfba656f9f3a3fd07b42be9df07
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-10 17:02:24 -0400

    Add files via upload

---
+++ src/test/java/com/speculation1000/specdb/start/SpecDbSuite.java
@@ -0,0 +1,19 @@
+package com.speculation1000.specdb.start;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.speculation1000.specdb.db.DbTest;
+import com.speculation1000.specdb.time.SpecDbDateTest;
+import com.speculation1000.specdb.time.SpecDbTimeTest;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   DbTest.class,
+   SpecDbDateTest.class,
+   SpecDbTimeTest.class,
+   StartRunTest.class
+})
+
+public class SpecDbSuite {}
