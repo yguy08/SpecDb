d1a338f4bc986984eaf083d38fb21104197a388d
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-11 17:17:37 -0400

    Update ExchangeDTO.java

--- src/main/java/com/speculation1000/specdb/dto/ExchangeDTO.java
+++ src/main/java/com/speculation1000/specdb/dto/ExchangeDTO.java
@@ -1,8 +1,6 @@
 package com.speculation1000.specdb.dto;
 
 import java.util.List;
-import java.util.Map;
-
 import com.speculation1000.specdb.market.Market;
 import com.speculation1000.specdb.start.SpecDbException;
 
@@ -10,7 +8,7 @@ public interface ExchangeDTO {
 	
 	List<Market> getLatestMarketList() throws SpecDbException;
 	
-	List<Market> fetchEntireExchangeHistory();
+	List<Market> fetchExchangeHistory(long endDate);
 	
 
 }
