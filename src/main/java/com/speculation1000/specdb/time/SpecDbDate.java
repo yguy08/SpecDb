657f37d79648901c0b29dfbb82db827810a75489
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-11 17:20:20 -0400

    Update SpecDbDate.java

--- src/main/java/com/speculation1000/specdb/time/SpecDbDate.java
+++ src/main/java/com/speculation1000/specdb/time/SpecDbDate.java
@@ -8,19 +8,26 @@ import java.time.format.DateTimeFormatter;
 
 public class SpecDbDate {
 	
-	public static long getTodayUtcEpochSeconds(Instant instant){
-		ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneId.of("Etc/UTC"));
-		ZonedDateTime today = ZonedDateTime.of(zdt.getYear(), zdt.getMonthValue(), zdt.getDayOfMonth(), 0, 0, 0, 0, ZoneId.of("Etc/UTC"));
-		return today.toEpochSecond();
+	/**
+	 * @param instant
+	 * @return instant rolled back to midnight epoch seconds 
+	 */
+	public static long getTodayMidnightEpochSeconds(Instant instant){
+		Instant todayMidnight = getTodayMidnightInstant(instant);
+		return todayMidnight.getEpochSecond();
 	}
 	
+	/**
+	 * @param instant
+	 * @return the instant rolled back to midnight (i.e 01/01/2017:10:15 -> 01/01/2017:0:00)
+	 */
 	public static Instant getTodayMidnightInstant(Instant instant){
 		ZonedDateTime zdt = ZonedDateTime.ofInstant(instant, ZoneId.of("Etc/UTC"));
 		ZonedDateTime today = ZonedDateTime.of(zdt.getYear(), zdt.getMonthValue(), zdt.getDayOfMonth(), 0, 0, 0, 0, ZoneId.of("Etc/UTC"));
 		return Instant.ofEpochSecond(today.toEpochSecond());
 	}
 	
-	/*
+	/**
 	 * @param Instant
 	 * @returns ISO_INSTANT Formatted String of Instant
 	 * Default method for log string Time stamps 
@@ -29,7 +36,7 @@ public class SpecDbDate {
 		return ZonedDateTime.ofInstant(instant, ZoneId.of("Etc/UTC")).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
 	}
 	
-	/*
+	/**
 	 * @param long
 	 * @returns ISO_INSTANT Formatted String of long
 	 * Default method for log string Time stamps 
@@ -51,14 +58,14 @@ public class SpecDbDate {
 		}
 	}
 	
-	/*
+	/**
 	 * @param instant
 	 * @returns yesterdays date in epoch seconds
 	 * Method converts the parameter to midnight (i.e. 01/01/17:0:01 -> 01/01/17:0:00)
 	 * Returns the midnight instant - 86400 (1 day seconds)
 	 */
 	public static long getYesterdayEpochSeconds(Instant instant){
-		long midnight = getTodayUtcEpochSeconds(instant);
+		long midnight = getTodayMidnightEpochSeconds(instant);
 		return midnight - 86400;
 	}
 	
@@ -70,8 +77,19 @@ public class SpecDbDate {
 		return initalDelay;
 	}
 	
+	/**
+	 * 
+	 * @param instant to compare
+	 * @returns days from today
+	 */
+	public static int daysFromInstantToToday(Instant instantToCompare){
+		Instant today = getTodayMidnightInstant(Instant.now());
+		long diff = today.getEpochSecond() - instantToCompare.getEpochSecond();
+		return (int) diff;
+	}
+	
 	public static void main(String[]args){
-		System.out.println("Today: " + getTodayUtcEpochSeconds(Instant.now()));
+		System.out.println("Today: " + getTodayMidnightEpochSeconds(Instant.now()));
 		System.out.println("Yesterday: " + getYesterdayEpochSeconds(Instant.now()));
 	}
 
