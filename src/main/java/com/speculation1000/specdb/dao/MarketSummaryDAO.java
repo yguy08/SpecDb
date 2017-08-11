78316ad16744d6efff555a23facfc6a9cf1a24e1
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-11 17:15:19 -0400

    Update MarketSummaryDAO.java

--- src/main/java/com/speculation1000/specdb/dao/MarketSummaryDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/MarketSummaryDAO.java
@@ -1,42 +1,43 @@
 package com.speculation1000.specdb.dao;
 
+import java.sql.Connection;
 import java.time.Instant;
 import java.util.List;
-import java.util.StringJoiner;
 
-import com.speculation1000.specdb.db.DbUtils;
+import com.speculation1000.specdb.db.QueryTable;
 import com.speculation1000.specdb.market.Market;
+import com.speculation1000.specdb.time.SpecDbDate;
 
 public class MarketSummaryDAO {
 	
 	public static List<Market> getAllLatest(){
 		String sqlCommand = "SELECT * FROM markets WHERE date = (SELECT Max(Date) from markets) order by symbol ASC";
-		List<Market> marketList = DbUtils.genericMarketQuery(sqlCommand);
+		List<Market> marketList = QueryTable.genericMarketQuery(sqlCommand);
 		return marketList;
 	}
 	
-	public static List<Market> getYesterdaysMarketsAfterCleanUp(Instant instant){
-		long yesterday = instant.getEpochSecond();
-		String sqlQuery = "SELECT * FROM markets WHERE date = " + yesterday;
-		List<Market> marketList = DbUtils.genericMarketQuery(sqlQuery);
-		return marketList;		
+	public static long getOldestRecordByExchange(Connection connection,String exchange){
+		String sqlCommand = "SELECT Min(Date) AS Date FROM markets WHERE exchange = " + "'"+exchange+"'";
+		List<Market> marketList = QueryTable.genericMarketQuery(connection, sqlCommand);
+		return marketList.get(0).getDate();
 	}
 	
-	public static String prettyMarketString(List<Market> marketList){
-		StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < marketList.size();i++){
-        	StringJoiner sj = new StringJoiner(":", "[", "]");
-        	for(int z = 0; z < 10; z++){
-        		sj.add(marketList.get(i).toString());
-        	}
-        	sb.append(sj.toString()+"\n");
-        }
-		return sb.toString();
+	public static List<Market> getLongEntries(int entryFlag){
+		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * entryFlag;
+		String sqlCommand = "SELECT m.* FROM markets m INNER JOIN " 
+							+ "(SELECT Symbol, Max(Close) Close FROM markets WHERE date > " + fromDate + " "
+							+ "GROUP BY Symbol) t ON m.Symbol = t.Symbol AND m.Close >= t.Close "
+							+ "WHERE date = (SELECT max(Date) from markets)";
+		List<Market> marketList = QueryTable.genericMarketQuery(sqlCommand);
+		return marketList;
 	}
+	
 	
 	public static void main(String[] args){
-		List<Market> marketList = MarketSummaryDAO.getAllLatest();
-		System.out.println(prettyMarketString(marketList));
+		List<Market> marketList = getLongEntries(25);
+		for(Market m : marketList){
+			System.out.println(m.toString());
+		}
 	}
 
 }
