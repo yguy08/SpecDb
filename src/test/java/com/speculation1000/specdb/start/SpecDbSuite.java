522cc836cadac873e8ab6875677b193c4e5d6eef
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-11 17:12:19 -0400

    Update SpecDbSuite.java

--- src/test/java/com/speculation1000/specdb/start/SpecDbSuite.java
+++ src/test/java/com/speculation1000/specdb/start/SpecDbSuite.java
@@ -3,6 +3,7 @@ package com.speculation1000.specdb.start;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.speculation1000.specdb.dao.MarketSummaryDAOTest;
 import com.speculation1000.specdb.db.DbTest;
 import com.speculation1000.specdb.time.SpecDbDateTest;
 import com.speculation1000.specdb.time.SpecDbTimeTest;
@@ -13,7 +14,8 @@ import com.speculation1000.specdb.time.SpecDbTimeTest;
    DbTest.class,
    SpecDbDateTest.class,
    SpecDbTimeTest.class,
-   StartRunTest.class
+   StartRunTest.class,
+   MarketSummaryDAOTest.class
 })
 
 public class SpecDbSuite {}
