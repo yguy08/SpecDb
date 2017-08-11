48c05485433fdbebf4bb3aa028f47437e5f7692a
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-11 17:11:29 -0400

    Add files via upload

---
+++ src/test/java/com/speculation1000/specdb/db/AbstractDbSetUpTest.java
@@ -0,0 +1,13 @@
+package com.speculation1000.specdb.db;
+
+public class AbstractDbSetUpTest {
+	
+	public static void setUpTestDb(){
+		
+	}
+	
+	public static void main(String[] args){
+		setUpTestDb();
+	}
+
+}
