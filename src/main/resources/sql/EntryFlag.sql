761247c06eed289e5824d04af403fe92b776ba62
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-14 17:06:26 -0400

    Delete EntryFlag.sql

--- src/main/resources/sql/EntryFlag.sql
+++
@@ -1,9 +0,0 @@
-SELECT m.*
-FROM markets m INNER JOIN
-	(
-		SELECT Symbol, Min(Close) Close
-		FROM markets		
-		WHERE date > 1501977600
-		GROUP BY Symbol
-	) t ON m.Symbol = t.Symbol AND m.Close >= t.Close
-	WHERE date = (SELECT max(Date) from markets)
