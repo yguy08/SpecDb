b3d89509630e0968408936d2ffa7616411eaa97f
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-14 17:06:54 -0400

    Add files via upload

---
+++ src/main/resources/sql/EntryFlag.sql
@@ -0,0 +1,5 @@
+SELECT m.* FROM markets m INNER JOIN
+							(SELECT Base,Counter, Exchange, Min(Close) Close FROM markets WHERE date > 1500508800
+							GROUP BY Base,Counter,Exchange) t ON m.Base = t.Base
+							AND m.Counter = t.Counter AND m.exchange = t.Exchange AND m.Close <= t.Close
+							WHERE date = (SELECT Max(Date) from markets)
