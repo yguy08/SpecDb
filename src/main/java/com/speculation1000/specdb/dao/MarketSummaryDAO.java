6c7909dfcf84f27a56ecedb5ff4e6625f2d8d4fc
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-14 17:05:44 -0400

    Update MarketSummaryDAO.java

--- src/main/java/com/speculation1000/specdb/dao/MarketSummaryDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/MarketSummaryDAO.java
@@ -1,5 +1,6 @@
 package com.speculation1000.specdb.dao;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.time.Instant;
 import java.util.List;
@@ -16,6 +17,12 @@ public class MarketSummaryDAO {
 		return marketList;
 	}
 	
+	public static List<Market> getAllMarketsFromDate(long day){
+		String sqlCommand = "SELECT * FROM markets WHERE date > " + day + " order by base ASC";
+		List<Market> marketList = QueryTable.genericMarketQuery(sqlCommand);
+		return marketList;
+	}
+	
 	public static long getOldestRecordByExchange(Connection connection,String exchange){
 		String sqlCommand = "SELECT Min(Date) AS Date FROM markets WHERE exchange = " + "'"+exchange+"'";
 		List<Market> marketList = QueryTable.genericMarketQuery(connection, sqlCommand);
@@ -24,21 +31,8 @@ public class MarketSummaryDAO {
 	
 	public static List<Market> getLongEntries(int entryFlag){
 		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * entryFlag;
-		String lastXDaysQuery;
-		List<Market> marketList = getLastXDays(5);
-		return marketList;
-	}
-	
-	public static List<Market> getLastXDays(int xDays){
-		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * xDays;
-		String lastXDaysSql = "SELECT * from markers where date > " + fromDate;
-		return null;
-	}
-	
-	public static List<Market> getShortEntries(int entryFlag){
-		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * entryFlag;
-		String sqlCommand = "SELECT m.* FROM markets m INNER JOIN " 
-							+ "(SELECT Base,Counter, Exchange, Min(Close) Close FROM markets WHERE date > " + fromDate + " "
+		String sqlCommand = "SELECT m.* FROM markets m INNER JOIN "
+							+ "(SELECT Base,Counter, Exchange, Max(Close) Close FROM markets WHERE date > " + fromDate + " "
 							+ "GROUP BY Base,Counter,Exchange) t ON m.Base = t.Base "
 							+ "AND m.Counter = t.Counter AND m.exchange = t.Exchange AND m.Close >= t.Close "
 							+ "WHERE date = (SELECT Max(Date) from markets)";
@@ -46,11 +40,39 @@ public class MarketSummaryDAO {
 		return marketList;
 	}
 	
+	public static BigDecimal getMaxClose(String base, String Counter, String Exchange, int days){
+		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * days;
+		String sqlCommand = "SELECT Max(Close) As Close FROM markets WHERE base = " + "'"+base+"'" + " "
+							+ "AND Counter = " + "'"+Counter+"'" + " "
+							+ "AND Exchange = " + "'"+Exchange+"'" + " "
+							+ "AND Date > " + fromDate;
+		List<Market> marketList = QueryTable.genericMarketQuery(sqlCommand);
+		return marketList.get(0).getClose();
+	}
+	
+	public static BigDecimal getMinClose(String base, String Counter, String Exchange, int days){
+		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * days;
+		String sqlCommand = "SELECT Min(Close) As Close FROM markets WHERE base = " + "'"+base+"'" + " "
+							+ "AND Counter = " + "'"+Counter+"'" + " "
+							+ "AND Exchange = " + "'"+Exchange+"'" + " "
+							+ "AND Date > " + fromDate;
+		List<Market> marketList = QueryTable.genericMarketQuery(sqlCommand);
+		return marketList.get(0).getClose();
+	}
+	
+	public static List<Market> getShortEntries(int entryFlag){
+		long fromDate = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()) - 86400 * entryFlag;
+		String sqlCommand = "SELECT m.* FROM markets m INNER JOIN "
+				+ "(SELECT Base,Counter, Exchange, Min(Close) Close FROM markets WHERE date > " + fromDate + " "
+				+ "GROUP BY Base,Counter,Exchange) t ON m.Base = t.Base "
+				+ "AND m.Counter = t.Counter AND m.exchange = t.Exchange AND m.Close <= t.Close "
+				+ "WHERE date = (SELECT Max(Date) from markets)";
+		List<Market> marketList = QueryTable.genericMarketQuery(sqlCommand);
+		return marketList;
+	}
+	
 	public static void main(String[] args){
-		List<Market> marketList = getLongEntries(25);
-		for(Market m : marketList){
-			System.out.println(m.toString());
-		}
+		
 	}
 
 }
