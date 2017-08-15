703418b9f099df71fcfea73836d95cce0185ef8a
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-15 17:21:52 -0400

    Add files via upload

---
+++ src/main/java/com/speculation1000/specdb/db/DbServer.java
@@ -0,0 +1,41 @@
+package com.speculation1000.specdb.db;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+
+public class DbServer {
+
+    public static void startDB() throws SQLException {
+        Server.createTcpServer("-tcpPort", "8082", "-tcpAllowOthers").start();
+    }
+
+    public static void stopDB() throws SQLException {
+        Server.shutdownTcpServer("tcp://localhost:8082", "", true, true);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("org.h2.Driver");
+
+            if (args.length > 0) {
+                if (args[0].trim().equalsIgnoreCase("start")) {
+                    startDB();
+                }
+
+                if (args[0].trim().equalsIgnoreCase("stop")) {
+                    stopDB();
+                }
+            } else {
+                System.err
+                        .println("Please provide one of following arguments: \n\t\tstart\n\t\tstop");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
