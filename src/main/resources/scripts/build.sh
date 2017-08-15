309b4e3f0f73aea448ea856bb947c7119ae42b8a
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-15 17:17:47 -0400

    Update build.sh

--- src/main/resources/scripts/build.sh
+++ src/main/resources/scripts/build.sh
@@ -8,16 +8,12 @@ TARGET=~/SpecDb/target
 
 echo removing target from $APP_DIR/target
 rm -r $APP_DIR/target
-rm -r $TMP_DIR
 
 cd $APP_DIR
 
 echo mvn install...
 mvn install
 echo mvn install complete
-
-mkdir $TMP_DIR
-cp -r $TARGET/spec-db-1.0.0.jar $TMP_DIR/specdb.jar 
 
 echo build complete!
 
