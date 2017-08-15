9868fcbb2c7b34947aeccca14909455205a0d1f1
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-15 17:16:18 -0400

    Update AbstractDbSetUpTest.java

--- src/test/java/com/speculation1000/specdb/db/AbstractDbSetUpTest.java
+++ src/test/java/com/speculation1000/specdb/db/AbstractDbSetUpTest.java
@@ -1,5 +1,12 @@
 package com.speculation1000.specdb.db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.speculation1000.specdb.market.Market;
+
 public class AbstractDbSetUpTest {
 	
 	public static void setUpTestDb(){
@@ -7,7 +14,37 @@ public class AbstractDbSetUpTest {
 	}
 	
 	public static void main(String[] args){
-		DbConnection.connect(DbConnectionEnum.H2_MAIN);
+		try {
+			DbServer.startDB();
+			System.out.println("db server started!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try{
+			Connection conn = DbConnection.connect(DbConnectionEnum.H2_MAIN);
+			List<Market> marketList = new ArrayList<>();
+			for(int i = 0; i < 10; i++){
+				Market m = new Market();
+				m.setBase("BTC");
+				m.setCounter("USDT");
+				m.setDate(159000000);
+				m.setExchange("POLO");
+				marketList.add(m);
+			}
+			
+			CreateTable.createTable(conn);
+			InsertRecord.insertBatchMarkets(conn, marketList);
+			String sql = "SELECT * from Markets";
+			List<Market> dbList = new ArrayList<>();
+			dbList = QueryTable.genericMarketQuery(conn, sql);
+			for(Market m : dbList){
+				System.out.println(m.toString());
+			}
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+		
 	}
 
 }
