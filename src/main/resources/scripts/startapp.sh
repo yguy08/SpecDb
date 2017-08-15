a4f13d9d18893dbc215dd276bdb7a17b68d265cd
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-15 17:18:24 -0400

    Update startapp.sh

--- src/main/resources/scripts/startapp.sh
+++ src/main/resources/scripts/startapp.sh
@@ -9,7 +9,7 @@ TARGET=~/SpecDb/target
 echo ssh to pi
 ssh pi@192.168.1.197 <<zzz23EndOfStatuszzz23
   cd /home/pi/SpecDb/
-  nohup java -cp tmp/specdb.jar com.speculation1000.specdb.start.StartApp $1 &
+  nohup java -cp target/specdb-1.0.jar com.speculation1000.specdb.start.StartApp $1 &
   ps -e | grep java
   echo Start up complete...CTRL-C to exit
 zzz23EndOfStatuszzz23
