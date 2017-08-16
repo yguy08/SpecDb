9c6f1aff17a5bf303e7fd84361047bad02104350
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-16 17:00:45 -0400

    Update StartRun.java

--- src/main/java/com/speculation1000/specdb/start/StartRun.java
+++ src/main/java/com/speculation1000/specdb/start/StartRun.java
@@ -2,6 +2,9 @@ package com.speculation1000.specdb.start;
 
 import java.time.Instant;
 
+import com.speculation1000.specdb.time.SpecDbDate;
+import com.speculation1000.specdb.time.SpecDbTime;
+
 public class StartRun {
 	
 	private static Instant startRunTS = null;
@@ -18,6 +21,41 @@ public class StartRun {
 			setStartRunTS();
 			return startRunTS;
 		}
+	}
+	
+	public static String getStartRunMessage(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append("********************************\n");
+        sb.append("          [" + StartApp.mode.getModeName() + "] \n");
+        sb.append("********************************\n");
+        sb.append("            [START]\n");
+        sb.append("* At: ");
+        sb.append(SpecDbDate.instantToLogStringFormat(StartRun.getStartRunTS()) + "\n");
+        sb.append("* New Day: " + SpecDbDate.isNewDay(StartRun.getStartRunTS()) + "\n");
+        sb.append("********************************\n");
+        return sb.toString();
+	}
+	
+	public static String getEndRunMessage(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append("********************************\n");
+        sb.append("          [" + StartApp.mode.getModeName() + "] \n");
+        Instant end = Instant.now();
+        sb.append("********************************\n");
+        sb.append("             [END]\n");
+        sb.append("* At: ");
+        sb.append(SpecDbDate.instantToLogStringFormat(end) + "\n");
+        sb.append("* Runtime: ");
+        sb.append(end.getEpochSecond() - StartRun.getStartRunTS().getEpochSecond() + " sec\n");
+        sb.append("********************************\n");
+        long i = SpecDbTime.getQuickModeDelaySeconds(Instant.now());
+        sb.append("* Next Update in " + i + " seconds\n");
+        sb.append("* H2 Db Server Status: \n");
+        sb.append("* " + DbServer.getH2ServerStatus() + " *\n");
+        sb.append("********************************\n");
+        return sb.toString();
 	}
 
 }
