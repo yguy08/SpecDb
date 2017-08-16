f338b86ed7363b372d615dd0a4059e4d06154482
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-16 12:09:25 -0400

    updated h2 main to localhost

--- src/main/java/com/speculation1000/specdb/db/DbConnectionEnum.java
+++ src/main/java/com/speculation1000/specdb/db/DbConnectionEnum.java
@@ -2,7 +2,7 @@ package com.speculation1000.specdb.db;
 
 public enum DbConnectionEnum {
 	
-	H2_MAIN("jdbc:h2:","tcp://192.168.1.197:8082/" + System.getProperty("user.home") + "/SpecDb/db/Speculation1000-H2","org.h2.Driver"),
+	H2_MAIN("jdbc:h2:","tcp://localhost:8082/" + System.getProperty("user.home") + "/SpecDb/db/Speculation1000-H2","org.h2.Driver"),
 	H2_TEST("jdbc:h2:",System.getProperty("user.home") + "/SpecDb/db/Speculation1000-H2Test","org.h2.Driver"),
 	SQLITE_MAIN("jdbc:sqlite:",System.getProperty("user.home") + "/SpecDb/db/Speculation1000.db","org.sqlite.JDBC"),
 	SQLITE_TEST("jdbc:sqlite:",System.getProperty("user.home") + "/SpecDb/db/Speculation1000-tmp.db","org.sqlite.JDBC");
@@ -24,8 +24,5 @@ public enum DbConnectionEnum {
 	public String getClassForName(){
 		return className;
 	}
-	
-	//h2 connection string help
-	//https://stackoverflow.com/questions/35854425/java-application-with-h2-database
 
 }
