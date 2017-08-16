a46b2e2b10dd9019b682b1e4562bbb8fddfb0640
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-16 12:08:36 -0400

    Update MarketSummaryDAO.java

--- src/main/java/com/speculation1000/specdb/dao/MarketSummaryDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/MarketSummaryDAO.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.Instant;
 import java.util.List;
-import java.util.StringJoiner;
 import java.util.logging.Level;
 
 import com.speculation1000.specdb.db.DbConnection;
@@ -98,29 +97,27 @@ public class MarketSummaryDAO {
 	}
 	
 	public static String getEntryStatus(){
-    	    List<Market> marketList = MarketSummaryDAO.getLongEntries(25);
-            StringBuilder sb = new StringBuilder();
-            sb.append("\n");
-            sb.append("********************************\n");
-            sb.append("          [ ENTRIES ]\n");
-            sb.append("********************************\n");
-            sb.append("          [ LONG ]\n");
-            sb.append("********************************\n");
-            for(Market market : marketList){
-                sb.append(market.toString()+"\n");
-            }
-            sb.append("          [ SHORT ]\n");
-            sb.append("********************************\n");
-            marketList = MarketSummaryDAO.getShortEntries(25);
-            for(Market market : marketList){
-                sb.append(market.toString()+"\n");
-            }
-            sb.append("********************************\n");
-            return sb.toString();		
+    	List<Market> marketList = MarketSummaryDAO.getLongEntries(25);
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append("********************************\n");
+        sb.append("          [ ENTRIES ]\n");
+        sb.append("********************************\n");
+        sb.append("          [ LONG ]\n");
+        sb.append("********************************\n");
+        for(Market market : marketList){
+            sb.append(market.toString()+"\n");
+        }
+        sb.append("          [ SHORT ]\n");
+        sb.append("********************************\n");
+        marketList = MarketSummaryDAO.getShortEntries(25);
+        for(Market market : marketList){
+            sb.append(market.toString()+"\n");
+        }
+        sb.append("********************************\n");
+        return sb.toString();		
 	}
 	
-	public static void main(String[] args){
-		
-	}
+	public static void main(String[] args){	}
 
 }
