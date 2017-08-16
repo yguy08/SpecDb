99567db7a04fa7df915e8afa4059913394fcfadf
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-16 16:59:56 -0400

    Update DbServer.java

--- src/main/java/com/speculation1000/specdb/start/DbServer.java
+++ src/main/java/com/speculation1000/specdb/start/DbServer.java
@@ -1,24 +1,41 @@
 package com.speculation1000.specdb.start;
 
 import java.sql.SQLException;
+import java.time.Instant;
+import java.util.logging.Level;
 
 import org.h2.tools.Server;
 
+import com.speculation1000.specdb.log.SpecDbLogger;
+
 public class DbServer {
 	
+	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
+	
 	public static Server server;
+	
+	protected static final Instant DB_START_UP_TS = Instant.now();
 
     public static void startDB() throws SQLException {
     	server = Server.createTcpServer("-tcpPort", "8082", "-tcpAllowOthers").start();
+    	specLogger.logp(Level.INFO, DbServer.class.getName(), "startDb", "H2 DbServer started");
     }
 
     public static void stopDB() throws SQLException {
         Server.shutdownTcpServer("tcp://localhost:8082", "", true, true);
+        specLogger.logp(Level.INFO, DbServer.class.getName(), "startDb", "H2 DbServer stopped");
     }
     
     public static String getH2ServerStatus(){
     	return server.getStatus();
     }
+    
+    /**
+	 * @returns seconds of db uptime
+	 */
+	public static long getSystemUptime(){
+		return Instant.now().getEpochSecond() - DB_START_UP_TS.getEpochSecond();
+	}
 
     public static void main(String[] args) {
 
@@ -43,5 +60,4 @@ public class DbServer {
         }
 
     }
-
 }
