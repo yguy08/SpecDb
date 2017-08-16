b9e49a634c167f51c567120b30c29ac7e4c78d23
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-16 16:58:29 -0400

    Update QuickMode.java

--- src/main/java/com/speculation1000/specdb/mode/QuickMode.java
+++ src/main/java/com/speculation1000/specdb/mode/QuickMode.java
@@ -2,7 +2,6 @@ package com.speculation1000.specdb.mode;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
-import java.time.Instant;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.logging.Level;
@@ -11,11 +10,10 @@ import com.speculation1000.specdb.dao.BittrexDAO;
 import com.speculation1000.specdb.dao.MarketSummaryDAO;
 import com.speculation1000.specdb.dao.PoloniexDAO;
 import com.speculation1000.specdb.log.SpecDbLogger;
-import com.speculation1000.specdb.start.DbServer;
 import com.speculation1000.specdb.start.SpecDbException;
 import com.speculation1000.specdb.start.StartRun;
+import com.speculation1000.specdb.start.SystemStatus;
 import com.speculation1000.specdb.time.SpecDbDate;
-import com.speculation1000.specdb.time.SpecDbTime;
 
 public class QuickMode implements Mode {
     
@@ -25,94 +23,67 @@ public class QuickMode implements Mode {
     
     private static final int PERIOD = 60 * 15;
     
-    @Override
-    public String getStartRunMessage() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n");
-        sb.append("********************************\n");
-        sb.append("          [ QUICKMODE ]\n");
-        sb.append("********************************\n");
-        sb.append("            [ START ]\n");
-        sb.append("* At: ");
-        sb.append(SpecDbDate.instantToLogStringFormat(StartRun.getStartRunTS()) + "\n");
-        sb.append("* New Day: " + SpecDbDate.isNewDay(StartRun.getStartRunTS()) + "\n");
-        sb.append("********************************\n");
-        return sb.toString();
-    }
-    
-    @Override
-    public String getEndRunMessage() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n");
-        sb.append("********************************\n");
-        sb.append("          [ QUICKMODE ]\n");
-        Instant end = Instant.now();
-        sb.append("********************************\n");
-        sb.append("          [ RESULTS ]\n");
-        sb.append("* Time: ");
-        sb.append(end.getEpochSecond() - StartRun.getStartRunTS().getEpochSecond() + " sec\n");
-        sb.append("********************************\n");
-        long i = SpecDbTime.getQuickModeDelaySeconds(Instant.now());
-        sb.append("* Next Update in " + i + " seconds\n");
-        sb.append("* H2 Db Server Status: \n");
-        sb.append("* " + DbServer.getH2ServerStatus() + " *\n");
-        sb.append("********************************\n");
-        return sb.toString();
-    }
+    private static final String modeNameStr = "QUICKMODE";
 
     @Override
-    public void startApp() {
+    public void startRun() {
         scheduler.scheduleAtFixedRate(new QuickMode(), 0, PERIOD, SECONDS);		
     }
 
     @Override
     public void run() {
         StartRun.setStartRunTS();
-        specLogger.logp(Level.INFO, QuickMode.class.getName(), "run",getStartRunMessage());
+        specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",StartRun.getStartRunMessage());
         
         PoloniexDAO polo = new PoloniexDAO();
         
         try{
             polo.updateMarkets();
         }catch(com.speculation1000.specdb.start.SpecDbException e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(), "run",e.getMessage());
+        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run",e.getMessage());
         }
         
         BittrexDAO bittrex = new BittrexDAO();
         try{
             bittrex.updateMarkets();
         }catch(SpecDbException e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(), "run",e.getMessage());
+        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run",e.getMessage());
         }
         
         if(SpecDbDate.isNewDay(StartRun.getStartRunTS())){
             try{
             	polo.cleanUpForNewDay();
-            	specLogger.logp(Level.INFO, QuickMode.class.getName(), "run", "Polo clean up successful");
+            	specLogger.logp(Level.INFO, QuickMode.class.getName(),"run","Polo clean up successful");
             }catch(SpecDbException e){
-            	specLogger.logp(Level.SEVERE, QuickMode.class.getName(), "run", "Error during Polo clean up");
+            	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error during Polo clean up");
             }
             
             try{
             	bittrex.cleanUpForNewDay();
-            	specLogger.logp(Level.INFO, QuickMode.class.getName(), "run", "Trex clean up successful");
+            	specLogger.logp(Level.INFO, QuickMode.class.getName(),"run","Trex clean up successful");
             }catch(SpecDbException e){
-            	specLogger.logp(Level.SEVERE, QuickMode.class.getName(), "run", "Error during TREX clean up");
+            	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error during TREX clean up");
             }
         }
         
         //restore
         try{
         	polo.restoreMarkets();
-        	specLogger.logp(Level.INFO, QuickMode.class.getName(), "run", "POLO restore successful");
+        	specLogger.logp(Level.INFO, QuickMode.class.getName(),"run","POLO restore successful");
         }catch(SpecDbException e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(), "run", "Error during POLO restore");
+        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error during POLO restore");
         }
         
-        specLogger.logp(Level.INFO, QuickMode.class.getName(), "run", MarketSummaryDAO.getEntryStatus());
+        specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",MarketSummaryDAO.getEntryStatus());
         
-        specLogger.logp(Level.INFO, QuickMode.class.getName(), "run",getEndRunMessage());        
+        specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",StartRun.getEndRunMessage());
+        
+        specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",SystemStatus.getSystemStatus());
+        
     }
 
-
+	@Override
+	public String getModeName() {
+		return modeNameStr;
+	}
 }
