e1bab740b17f7e81c161a5b9cea8f5a791c8059e
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-23 17:54:35 -0400

    Update StandardMode.java

--- src/main/java/com/speculation1000/specdb/mode/StandardMode.java
+++ src/main/java/com/speculation1000/specdb/mode/StandardMode.java
@@ -12,7 +12,7 @@ import com.speculation1000.specdb.db.DbConnectionEnum;
 import com.speculation1000.specdb.log.SpecDbLogger;
 import com.speculation1000.specdb.start.SpecDbException;
 import com.speculation1000.specdb.start.StartRun;
-import com.speculation1000.specdb.start.SystemStatus;
+import com.speculation1000.specdb.start.StatusString;
 import com.speculation1000.specdb.time.SpecDbTime;
 
 public class StandardMode implements Mode {
@@ -35,39 +35,31 @@ public class StandardMode implements Mode {
     @Override
     public void run() {
         StartRun.setStartRunTS();
-        specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",StartRun.getStartRunMessage());
+        specLogger.logp(Level.INFO, StandardMode.class.getName(),"run",StartRun.getStartRunMessage());
                 
         try{
         	MarketSummaryDAO.updateTickerList(DbConnectionEnum.H2_MAIN);
-        	specLogger.logp(Level.INFO, QuickMode.class.getName(),"run","update successful");
+        	specLogger.logp(Level.INFO, StandardMode.class.getName(),"run","update successful");
         }catch(SpecDbException e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run",e.getMessage());
+        	specLogger.logp(Level.SEVERE, StandardMode.class.getName(),"run",e.getMessage());
         }
         
         //restore
         try{
         	MarketSummaryDAO.restoreMarkets();
-        	specLogger.logp(Level.INFO, QuickMode.class.getName(),"run","Restore successful");
+        	specLogger.logp(Level.INFO, StandardMode.class.getName(),"run","Restore successful");
         }catch(SpecDbException e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error during restore");
+        	specLogger.logp(Level.SEVERE, StandardMode.class.getName(),"run","Error during restore");
         }
         
         try{
-            specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",MarketSummaryDAO.getEntryStatus());
+            specLogger.logp(Level.INFO, StandardMode.class.getName(),"run",StatusString.getTickerString());
+            specLogger.logp(Level.INFO, StandardMode.class.getName(),"run",StartRun.getEndRunMessage());
+            specLogger.logp(Level.INFO, StandardMode.class.getName(), "run", StatusString.getLongEntriesString());
+            specLogger.logp(Level.INFO, StandardMode.class.getName(), "run", StatusString.getShortEntriesString());
+            specLogger.logp(Level.INFO, StandardMode.class.getName(),"run",StatusString.getSystemStatus());
         }catch(Exception e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error getting entry status");
-        }
-        
-        try{
-            specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",StartRun.getEndRunMessage());
-        }catch(Exception e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error getting end run status");
-        }
-        
-        try{
-            specLogger.logp(Level.INFO, QuickMode.class.getName(),"run",SystemStatus.getSystemStatus());
-        }catch(Exception e){
-        	specLogger.logp(Level.SEVERE, QuickMode.class.getName(),"run","Error getting system status");
+        	specLogger.logp(Level.SEVERE, StandardMode.class.getName(),"run","Error getting status messages");
         }
     }
 
