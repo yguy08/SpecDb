21085c128856cb210dc30c0d959ee596fff74e43
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-24 16:36:19 -0400

    Update StatusString.java

--- src/main/java/com/speculation1000/specdb/start/StatusString.java
+++ src/main/java/com/speculation1000/specdb/start/StatusString.java
@@ -19,18 +19,14 @@ public class StatusString {
         sb.append("          [SYSTEMSTATUS] \n");
         sb.append("* At: ");
         sb.append(SpecDbDate.instantToLogStringFormat(now) + "\n");
-        sb.append("********************************\n");
-        sb.append("                [APP]          \n");
         sb.append("* App Running since: ");
         sb.append(SpecDbDate.instantToLogStringFormat(StartApp.getStartUpTs()));
-        sb.append("* App Uptime: " + SpecDbTime.uptimePrettyStr(StartApp.getSystemUptime()) + "\n");
-        sb.append("********************************\n");
-        sb.append("                [H2 DB]          \n");
+        sb.append(" (Uptime: " + SpecDbTime.uptimePrettyStr(StartApp.getSystemUptime()) + ")\n");
         sb.append("* H2 DB Running since: ");
         sb.append(SpecDbDate.instantToLogStringFormat(DbServer.DB_START_UP_TS));
-        sb.append("* H2 DB Uptime: " + SpecDbTime.uptimePrettyStr(DbServer.getSystemUptime()) + "\n");
-        sb.append("* H2 DB Status: \n");
-        sb.append("* " + DbServer.getH2ServerStatus() + " *\n");
+        sb.append("(Uptime: " + SpecDbTime.uptimePrettyStr(DbServer.getSystemUptime()) + ")\n");
+        sb.append("* H2 DB Status: ");
+        sb.append(DbServer.getH2ServerStatus() + " *\n");
         sb.append("********************************\n");
         return sb.toString();
 	}
