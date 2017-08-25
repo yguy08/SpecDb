41a0f73ed2620fab8b324e9e30c3b8d53c37a558
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-25 15:07:08 -0400

    Update deploy.sh

--- src/main/resources/scripts/deploy.sh
+++ src/main/resources/scripts/deploy.sh
@@ -7,18 +7,18 @@ echo *****************
 APP_DIR=~/SpecDb
 TARGET=~/SpecDb/target
 TMP_DIR=~/SpecDb/tmp
-SCRIPTS=~/SpecDb/src/main/rescources/scripts
+SCRIPTS=~/SpecDb/src/main/resources/scripts
 
 echo cleaning up raspberry pi
 ssh pi@192.168.1.151 <<zzz23EndOfStatuszzz23
   echo stopping h2 server!
   cd /home/pi/SpecDb/
-  java -cp target/specdb-1.0.jar com.speculation1000.specdb.start.DbServer stop
-  sleep 20
+  java -cp target/specdb-1.0.jar org.h2.tools.Server -tcpShutdown tcp://localhost:8082
+  sleep 10
   echo h2 server stopped!
   echo Killing java process on Raspberry pi...
   killall -w java
-  sleep 20
+  sleep 10
   echo java processes stopped!
   ps -e | grep java  
   echo Removing specdb jar from pi
