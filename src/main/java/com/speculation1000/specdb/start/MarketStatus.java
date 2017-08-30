cf46968ee0fa858ca31a0b48b7304a62daeae34f
Wyatt Endres <wyatt.endres@gmail.com>
2017-08-30 16:57:14 -0400

    Add files via upload

---
+++ src/main/java/com/speculation1000/specdb/start/MarketStatus.java
@@ -0,0 +1,52 @@
+package com.speculation1000.specdb.start;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+import com.speculation1000.specdb.dao.MarketSummaryDAO;
+import com.speculation1000.specdb.db.DbConnectionEnum;
+import com.speculation1000.specdb.log.SpecDbLogger;
+import com.speculation1000.specdb.market.MarketStatusContent;
+
+public class MarketStatus {
+
+	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
+	
+	private static List<MarketStatusContent> marketStatusList;
+	
+	public static void updateMarketStatusList(){
+		try {
+			marketStatusList = MarketSummaryDAO.getMarketStatusList(DbConnectionEnum.H2_MAIN);
+			specLogger.logp(Level.INFO, MarketStatus.class.getName(), "updateMarketStatusList", "Retrieved market status list!");
+		} catch (SpecDbException e) {
+			specLogger.logp(Level.SEVERE, MarketStatus.class.getName(), "updateMarketStatusList", "Failed to retrieve market status list");
+		}
+	}
+	
+	public static List<MarketStatusContent> getMarketStatusList(){
+		return marketStatusList;
+	}
+	
+	public static void main(String[]args){
+		updateMarketStatusList();
+		for(MarketStatusContent msc : marketStatusList){
+			System.out.println(msc.getSymbol());
+			for(Map.Entry<Long,BigDecimal> e : msc.getClosePriceMap().entrySet()){
+				System.out.print(e.getKey() + " " + e.getValue() + " ");
+			}
+			System.out.println("");
+			for(Map.Entry<Long,Integer> e : msc.getDayHighLowMap().entrySet()){
+				System.out.print(e.getKey() + " " + e.getValue() + " ");
+			}
+			System.out.println("");
+			for(Map.Entry<Long,BigDecimal> e : msc.getAtrMap().entrySet()){
+				System.out.print(e.getKey() + " " + e.getValue() + " ");
+			}
+			System.out.println("");
+		}
+	}
+
+	
+}
