a3ec77d585994257a5c95716fb236132d6dd370f
Wyatt <Wyatt.endres@gmail.com>
2017-08-31 20:01:31 -0400

    stop app .sh

--- src/main/resources/scripts/stopapp.sh
+++ src/main/resources/scripts/stopapp.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-echo stopping app
 
+APP_DIR=~/SpecDb
+TARGET=~/SpecDb/target
+TMP_DIR=~/SpecDb/tmp
+SCRIPTS=~/SpecDb/src/main/resources/scripts
+
+echo cleaning up raspberry pi
 ssh pi@192.168.1.151 <<zzz23EndOfStatuszzz23
   echo stopping h2 server!
   cd /home/pi/SpecDb/
-  java -cp target/specdb-1.0.jar com.speculation1000.specdb.start.DbServer stop
+  java -cp target/specdb-1.0.jar org.h2.tools.Server -tcpShutdown tcp://localhost:8082
+  sleep 10
   echo h2 server stopped!
-  echo stopping java process
+  echo Killing java process on Raspberry pi...
   killall -w java
-  echo java process stopped!
+  sleep 10
+  echo java processes stopped!
+  ps -e | grep java
+  echo Cleaning up old log file
+  rm -r /home/pi/SpecDb/logs/Spec*
   exit
 zzz23EndOfStatuszzz23
 
-echo app stopped
+echo Raspberry pi cleaned up...
 
 exit
