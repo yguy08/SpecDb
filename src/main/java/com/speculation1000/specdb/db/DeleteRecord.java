616b97760b0e22bcca6b4a54989b844704789de0
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-05 16:49:03 -0400

    Update DeleteRecord.java

--- src/main/java/com/speculation1000/specdb/db/DeleteRecord.java
+++ src/main/java/com/speculation1000/specdb/db/DeleteRecord.java
@@ -3,7 +3,6 @@ package com.speculation1000.specdb.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.logging.Level;
 
 import com.speculation1000.specdb.log.SpecDbLogger;
@@ -11,21 +10,6 @@ import com.speculation1000.specdb.log.SpecDbLogger;
 public class DeleteRecord {
 	
 	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
-	
-	public static int deleteRecords(Connection connection, String strSql){
-        try {
-            Statement tmpStatement = connection.createStatement();
-            int result = tmpStatement.executeUpdate(strSql);
-            tmpStatement.close();
-            return result;
-        } catch (SQLException ex) {
-        	while (ex != null) {
-            	specLogger.logp(Level.INFO, DeleteRecord.class.getName(), "deleteRecords", "SQLException: " + ex.getMessage());
-	            ex = ex.getNextException();
-	        }
-	        throw new RuntimeException("Error");
-        }
-	}
 	
 	public static int[] deleteBulkMarkets(DbConnectionEnum dbce, long date){
 		Connection conn = DbConnection.connect(dbce);
@@ -40,11 +24,31 @@ public class DeleteRecord {
 			return results;
 		}catch(SQLException ex){
         	while (ex != null) {
-            	specLogger.logp(Level.INFO, DeleteRecord.class.getName(), "deleteRecords", "SQLException: " + ex.getMessage());
+            	specLogger.logp(Level.INFO, DeleteRecord.class.getName(), "deleteBulkMarkets", "SQLException: " + ex.getMessage());
 	            ex = ex.getNextException();
 	        }
 	        throw new RuntimeException("Error");
 		}
+	}
+	
+	public static int[] deleteAccountRecords(DbConnectionEnum dbce, long date){
+		Connection conn = DbConnection.connect(dbce);
+		try{
+			String deleteSql = "DELETE FROM account WHERE Date = ?";              
+			PreparedStatement st = conn.prepareStatement(deleteSql);
+			st.setLong(1, date);
+			st.addBatch();
+			int[] results = st.executeBatch();
+			st.close();
+			conn.close();
+			return results;
+		}catch(SQLException ex){
+        	while (ex != null) {
+            	specLogger.logp(Level.INFO, DeleteRecord.class.getName(), "deleteAccountRecords", "SQLException: " + ex.getMessage());
+	            ex = ex.getNextException();
+	        }
+	        throw new RuntimeException("Error");
+		}		
 	}
 
 
