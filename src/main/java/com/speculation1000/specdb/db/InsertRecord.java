7251f546b230e363769d51f1030d409c43507539
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-06 18:15:51 -0400

    Update InsertRecord.java

--- src/main/java/com/speculation1000/specdb/db/InsertRecord.java
+++ src/main/java/com/speculation1000/specdb/db/InsertRecord.java
@@ -15,42 +15,6 @@ public class InsertRecord {
 	
 	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
 	
-	public static void insertBatchMarkets(Connection connection, List<Market> marketList){
-		String sqlCommand = "INSERT INTO markets(Base,Counter,Exchange,Date,High,Low,Open,Close,Volume) VALUES(?,?,?,?,?,?,?,?,?)";
-        try {
-            PreparedStatement tmpStatement = connection.prepareStatement(sqlCommand);
-	        for(int i = 0; i < marketList.size();i++){
-        		Market m = marketList.get(i);
-        		tmpStatement.setString(1, m.getBase());
-        		tmpStatement.setString(2, m.getCounter());
-        		tmpStatement.setString(3, m.getExchange());
-        		tmpStatement.setLong(4,m.getDate());
-        		tmpStatement.setBigDecimal(5, m.getHigh());
-        		tmpStatement.setBigDecimal(6, m.getLow());
-        		tmpStatement.setBigDecimal(7,m.getOpen());
-        		tmpStatement.setBigDecimal(8, m.getClose());
-        		tmpStatement.setInt(9,m.getVolume());
-        		tmpStatement.addBatch();
-        	if((i % 10000 == 0 && i != 0) || i == marketList.size() - 1){
-        		specLogger.logp(Level.INFO, InsertRecord.class.getName(),"insertBatchMarkets", "Adding batch: " + i);
-        		long start = System.currentTimeMillis();
-        		tmpStatement.executeBatch();
-    	        long end = System.currentTimeMillis();
-    	        specLogger.logp(Level.INFO, InsertRecord.class.getName(),"insertBatchMarkets", "total time taken to insert the batch = " + (end - start) + " ms");
-        	}
-        }
-            tmpStatement.close();
-        } catch (SQLException ex) {
-        	StringBuffer sb = new StringBuffer();
-	        sb.append("SQLException information\n");
-	        while (ex != null) {
-	            sb.append("Error msg: " + ex.getMessage() + "\n");
-	            ex = ex.getNextException();
-	        }
-	        throw new RuntimeException("Error");
-        }
-	}
-	
 	public static void insertBatchMarkets(DbConnectionEnum dbce, List<Market> marketList){
 		String sqlCommand = "INSERT INTO markets(Base,Counter,Exchange,Date,High,Low,Open,Close,Volume) VALUES(?,?,?,?,?,?,?,?,?)";
         try {
@@ -77,6 +41,7 @@ public class InsertRecord {
         	}
         }
             tmpStatement.close();
+            connection.close();
         } catch (SQLException ex) {
         	StringBuffer sb = new StringBuffer();
 	        sb.append("SQLException information\n");
@@ -97,6 +62,7 @@ public class InsertRecord {
 	        tmpStatement.setBigDecimal(2, balance);
     		tmpStatement.execute();
 	        tmpStatement.close();
+	        connection.close();
         } catch (SQLException ex) {
         	StringBuffer sb = new StringBuffer();
 	        sb.append("SQLException information\n");
@@ -135,6 +101,7 @@ public class InsertRecord {
         	}
         }
             tmpStatement.close();
+            connection.close();
         } catch (SQLException ex) {
         	StringBuffer sb = new StringBuffer();
 	        sb.append("SQLException information\n");
