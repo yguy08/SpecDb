5ea035f2fdc9d462e883562e0335c8c38385a5ab
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-06 18:15:34 -0400

    Update DropTable.java

--- src/main/java/com/speculation1000/specdb/db/DropTable.java
+++ src/main/java/com/speculation1000/specdb/db/DropTable.java
@@ -13,13 +13,15 @@ public class DropTable {
 	
 	private static final String DROP_TABLE = DropTable.class.getName();
 	
-	public static void dropTable(Connection connection){
+	public static void dropMarketTable(DbConnectionEnum dbce){
 		String sql = "DROP TABLE IF EXISTS market";
         try {
+        	Connection connection = DbConnection.connect(dbce);
             Statement tmpStatement = connection.createStatement();
             tmpStatement.executeUpdate(sql);
             specLogger.logp(Level.INFO, DROP_TABLE, "dropTable", "Table dropped!");
             tmpStatement.close();
+            connection.close();
         } catch (SQLException ex) {
 	        System.err.println("SQLException information");
 	        while (ex != null) {
