581e00c00e547e3e0a05a8dc4d8ef652edc61591
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-12 17:41:28 -0400

    Update MarketUtils.java

--- src/main/java/com/speculation1000/specdb/market/MarketUtils.java
+++ src/main/java/com/speculation1000/specdb/market/MarketUtils.java
@@ -6,31 +6,20 @@ import java.util.List;
 
 public class MarketUtils {
 	
-	public static boolean isXDayHigh(List<BigDecimal> closePriceList, int days) {
+	public static int xDayHighLow(List<BigDecimal> closePriceList, int days){
 		if(closePriceList.size() < days){
 			days = closePriceList.size();
 		}
 		BigDecimal currentPrice = closePriceList.get(0);
 		List<BigDecimal> subList = closePriceList.subList(0, days);
 		BigDecimal max = Collections.max(subList);
-		if(currentPrice.compareTo(max)>=0){
-			return true;
-		}else{
-			return false;
-		}
-	}
-	
-	public static boolean isXDayLow(List<BigDecimal> closePriceList, int days) {
-		if(closePriceList.size() < days){
-			days = closePriceList.size();
-		}
-		BigDecimal currentPrice = closePriceList.get(0);
-		List<BigDecimal> subList = closePriceList.subList(0, days);
 		BigDecimal min = Collections.min(subList);
-		if(currentPrice.compareTo(min)<=0){
-			return true;
+		if(currentPrice.compareTo(max)>=0){
+			return days;
+		}else if(currentPrice.compareTo(min)<=0){
+			return -(days);
 		}else{
-			return false;
+			return 0;
 		}
 	}
 
