0400d3f444b81bd7e3be1e3f1c54e3ef9deb85e1
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-12 17:37:30 -0400

    Update BittrexDAO.java

--- src/main/java/com/speculation1000/specdb/dao/BittrexDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/BittrexDAO.java
@@ -9,6 +9,7 @@ import com.speculation1000.specdb.db.DbConnectionEnum;
 import com.speculation1000.specdb.dto.BittrexDTO;
 import com.speculation1000.specdb.log.SpecDbLogger;
 import com.speculation1000.specdb.market.Market;
+import com.speculation1000.specdb.market.MarketEntry;
 
 public class BittrexDAO implements ExchangeDAO {
 
@@ -33,6 +34,12 @@ public class BittrexDAO implements ExchangeDAO {
 
 	@Override
 	public BigDecimal getAccountBalance(DbConnectionEnum dbce) throws SpecDbException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<MarketEntry> getEntries(DbConnectionEnum dbce, int days) throws SpecDbException {
 		// TODO Auto-generated method stub
 		return null;
 	}
