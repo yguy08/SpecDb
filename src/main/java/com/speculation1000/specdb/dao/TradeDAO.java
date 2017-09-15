9279faa85b095b43812c730eabe9ea60de4c04ec
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-15 15:52:44 -0400

    Update TradeDAO.java

--- src/main/java/com/speculation1000/specdb/dao/TradeDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/TradeDAO.java
@@ -1,18 +1,24 @@
 package com.speculation1000.specdb.dao;
 
+import java.math.BigDecimal;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Level;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.speculation1000.specdb.db.DbConnectionEnum;
-import com.speculation1000.specdb.db.DeleteRecord;
-import com.speculation1000.specdb.db.InsertRecord;
-import com.speculation1000.specdb.db.QueryTable;
+import com.speculation1000.specdb.db.DbUtils;
 import com.speculation1000.specdb.log.SpecDbLogger;
-import com.speculation1000.specdb.market.MarketEntry;
+import com.speculation1000.specdb.market.Market;
+import com.speculation1000.specdb.market.Symbol;
 import com.speculation1000.specdb.start.SpecDbException;
 import com.speculation1000.specdb.time.SpecDbDate;
+import com.speculation1000.specdb.trade.TradeStatusEnum;
 
 public class TradeDAO {
 	
@@ -21,33 +27,53 @@ public class TradeDAO {
 	public TradeDAO(DbConnectionEnum dbce, int days) throws SpecDbException {
 		//update entries
 		try{
-			updateEntries(dbce, days);
+			updateTrades(dbce, days);
 		}catch(Exception e){
 			specLogger.logp(Level.SEVERE, TradeDAO.class.getName(),"TradeDAO","Error updating entries");
 			throw new SpecDbException(e.getMessage());
 		}
 	}
 	
-	private void updateEntries(DbConnectionEnum dbce, int days) throws SpecDbException {
-		List<MarketEntry> poloMarketEntries = new ArrayList<>();
-		long todayMidnight = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now());
+	private void updateTrades(DbConnectionEnum dbce, int days) throws SpecDbException {
+		BigDecimal accountBalance = AccountDAO.getCurrentAccountBalance(dbce);
 		
+		List<Market> marketList = new ArrayList<>();
+		Market market;
 		try{
-			poloMarketEntries = new PoloniexDAO().getEntries(dbce, days);
+			List<Symbol> symbolList = DbUtils.getMarketHighs(dbce, days).stream().map(Market::getSymbol).collect(Collectors.toList());
+			TreeMap<Symbol,List<Market>> marketMap = MarketDAO.getSelectMarketMap(dbce, 150, symbolList);
+			for(Map.Entry<Symbol, List<Market>> e : marketMap.entrySet()){
+				market = Market.createNewMarketTrade(e.getKey(), e.getValue(),accountBalance,TradeStatusEnum.LONG);
+				marketList.add(market);
+			}
+			specLogger.logp(Level.INFO, TradeDAO.class.getName(),"TradeDAO","Found new trades (long)");
 		}catch(Exception e){
-			
+			specLogger.logp(Level.SEVERE, TradeDAO.class.getName(),"updateTrades","Error getting market highs");
 		}
 		
-		//ADD: bittrex		
 		try{
-			DeleteRecord.deleteEntries(dbce, todayMidnight);			
+			List<Symbol> symbolList = DbUtils.getMarketLows(dbce, days).stream().map(Market::getSymbol).collect(Collectors.toList());
+			TreeMap<Symbol,List<Market>> marketMap = MarketDAO.getSelectMarketMap(dbce, 150, symbolList);
+			for(Map.Entry<Symbol, List<Market>> e : marketMap.entrySet()){
+				market = Market.createNewMarketTrade(e.getKey(), e.getValue(),accountBalance,TradeStatusEnum.SHORT);
+				marketList.add(market);
+			}
+			specLogger.logp(Level.INFO, TradeDAO.class.getName(),"TradeDAO","Found new trades (short)");
+		}catch(Exception e){
+			specLogger.logp(Level.SEVERE, TradeDAO.class.getName(),"updateTrades","Error getting market lows");
+		}		
+		
+		try{
+			int[] numDeleted = DbUtils.deleteNewTrades(dbce,SpecDbDate.getTodayMidnightEpochSeconds(Instant.now()));
+			int sum = IntStream.of(numDeleted).sum();
+			specLogger.logp(Level.INFO, TradeDAO.class.getName(),"TradeDAO","Cleaned up today's new trades (Deleted: "+sum+")");
 		}catch(Exception e){
 			specLogger.logp(Level.SEVERE, MarketDAO.class.getName(),"updateEntries","Error cleaning up today's entries");
 			throw new SpecDbException(e.getMessage());
 		}
 		
 		try{
-			InsertRecord.insertBatchEntries(dbce, poloMarketEntries);
+			DbUtils.insertNewTrades(dbce, marketList);
 		}catch(Exception e){
 			specLogger.logp(Level.SEVERE, MarketDAO.class.getName(),"updateEntries","Error inserting new polo entries");
 			throw new SpecDbException(e.getMessage());
@@ -55,13 +81,10 @@ public class TradeDAO {
 		
 	}
 	
-	public static List<MarketEntry> getMarketEntryList(DbConnectionEnum dbce,int days){
-		Instant instant = SpecDbDate.getTodayMidnightInstant(Instant.now().minusSeconds(86400 * days));
-		String sqlCommand = "SELECT * FROM entry WHERE Date >= " + SpecDbDate.getTodayMidnightEpochSeconds(instant)
-				+ " GROUP BY Symbol,Date"
-				+ " ORDER BY Symbol ASC, Date DESC";
-		List<MarketEntry> marketEntryList = QueryTable.genericEntryQuery(dbce, sqlCommand);
-		return marketEntryList;
+	public static List<Market> getMarketEntryList(DbConnectionEnum dbce){
+		List<Market> marketList = DbUtils.getNewTrades(dbce);
+		Collections.sort(marketList);
+		return marketList;
 	}
 	
 	
