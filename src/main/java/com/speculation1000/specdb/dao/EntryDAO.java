f08d0b284c14763b32d2149959a1d491b3fffac1
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-15 15:58:34 -0400

    Add files via upload

---
+++ src/main/java/com/speculation1000/specdb/dao/EntryDAO.java
@@ -0,0 +1,9 @@
+package com.speculation1000.specdb.dao;
+
+import com.speculation1000.specdb.log.SpecDbLogger;
+
+public class EntryDAO {
+	
+	private static final SpecDbLogger specLogger = SpecDbLogger.getSpecDbLogger();
+
+}
