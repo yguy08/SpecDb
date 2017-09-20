51a43c5936e62c2c4b0c39dd9489382ca333b16c
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-20 17:27:51 -0400

    Update AccountDAO.java

--- src/main/java/com/speculation1000/specdb/dao/AccountDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/AccountDAO.java
@@ -1,18 +1,17 @@
 package com.speculation1000.specdb.dao;
 
 import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.TreeMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import com.speculation1000.specdb.db.DbConnectionEnum;
-import com.speculation1000.specdb.db.DeleteRecord;
-import com.speculation1000.specdb.db.InsertRecord;
-import com.speculation1000.specdb.db.QueryTable;
+import com.speculation1000.specdb.db.DbUtils;
 import com.speculation1000.specdb.log.SpecDbLogger;
+import com.speculation1000.specdb.market.AccountBalance;
 import com.speculation1000.specdb.start.SpecDbException;
+import com.speculation1000.specdb.start.StandardMode;
 import com.speculation1000.specdb.time.SpecDbDate;
-import com.speculation1000.specdb.utils.SpecDbNumFormat;
 
 public class AccountDAO {
 	
@@ -30,38 +29,31 @@ public class AccountDAO {
 	}
 	
 	public void updateAccountBalance(DbConnectionEnum dbce) throws SpecDbException{
-		long todayMidnight = SpecDbDate.getTodayMidnightEpochSeconds(Instant.now());
-		BigDecimal poloBalance;
+		long todayMidnight = SpecDbDate.getTodayMidnightEpochSeconds(StandardMode.getStartRunTS());
+		List<AccountBalance> balanceList = new ArrayList<>();
 		try{
-			poloBalance = new PoloniexDAO().getAccountBalance(dbce);
+			balanceList.addAll(new PoloniexDAO().getAccountBalance(dbce));
 			specLogger.logp(Level.INFO, AccountDAO.class.getName(),"updateAccountBalance","AccountBalance");
 		}catch(Exception e){
 			specLogger.logp(Level.SEVERE, AccountDAO.class.getName(),"AccountDAO","Error updating polo account balance");
 			throw new SpecDbException(e.getMessage());
 		}		
 		
-		//add trex
-		BigDecimal balance = SpecDbNumFormat.bdToEightDecimal(poloBalance); //plus trexBalance when implemented
-		DeleteRecord.deleteAccountRecords(dbce, todayMidnight);
-		InsertRecord.insertAccountBalance(dbce, balance, todayMidnight);
+		//Clean up old ones from today
+		DbUtils.accountBalCleanUp(dbce,todayMidnight);
+		
+		//Insert updated balances
+		DbUtils.insertUpdatedAccountBalances(dbce,balanceList);
 	}
 	
 	public static BigDecimal getCurrentAccountBalance(DbConnectionEnum dbce) throws SpecDbException {
-		String sqlCommand = "SELECT * FROM Account WHERE Date = (SELECT Max(Date) As Date FROM Account)";
-		TreeMap<Long,BigDecimal> accountMap = QueryTable.genericAccountQuery(dbce, sqlCommand);
-        specLogger.logp(Level.INFO, AccountDAO.class.getName(), "getAccountBalance", "Got account balance!");
-        //get bittrex balance        
-		return accountMap.firstEntry().getValue();
-	}
-	
-	public static TreeMap<Long,BigDecimal> getAccountBalanceMap(DbConnectionEnum dbce,int days) throws SpecDbException {
-		Instant instant = Instant.now().minusSeconds(86400 * days);
-		String sqlCommand = "SELECT * FROM Account WHERE Date >= " + SpecDbDate.getTodayMidnightEpochSeconds(instant)
-							+ " ORDER BY DATE DESC";
-		TreeMap<Long,BigDecimal> accountMap = QueryTable.genericAccountQuery(dbce, sqlCommand);
-        specLogger.logp(Level.INFO, AccountDAO.class.getName(), "getAccountBalance", "Got account balance!");
-        //get bittrex balance
-		return accountMap;
+		List<AccountBalance> accountBalList = DbUtils.getLatestAccountBalances(dbce);
+		BigDecimal bal = new BigDecimal(0.00);
+		for(AccountBalance ab : accountBalList){
+			bal = bal.add(ab.getAmount());
+		}
+        specLogger.logp(Level.INFO, AccountDAO.class.getName(), "getAccountBalance", "Got account balance!");        
+		return bal;
 	}
 
 }
