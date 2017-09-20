86efae754f521108f029c777f1135164f67b3573
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-20 17:28:25 -0400

    Update ExchangeDAO.java

--- src/main/java/com/speculation1000/specdb/dao/ExchangeDAO.java
+++ src/main/java/com/speculation1000/specdb/dao/ExchangeDAO.java
@@ -1,9 +1,9 @@
 package com.speculation1000.specdb.dao;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import com.speculation1000.specdb.db.DbConnectionEnum;
+import com.speculation1000.specdb.market.AccountBalance;
 import com.speculation1000.specdb.market.Market;
 import com.speculation1000.specdb.start.SpecDbException;
 
@@ -13,6 +13,6 @@ public interface ExchangeDAO {
 		
 	void restoreMarkets(DbConnectionEnum dbce) throws SpecDbException;
 	
-	BigDecimal getAccountBalance(DbConnectionEnum dbce) throws SpecDbException;
+	List<AccountBalance> getAccountBalance(DbConnectionEnum dbce) throws SpecDbException;
 
 }
