a5ecbaba0f3acbcc79cae101df55ec81bfc0313e
Wyatt Endres <wyatt.endres@gmail.com>
2017-09-21 17:04:19 -0400

    Update AccountBalance.java

--- src/main/java/com/speculation1000/specdb/market/AccountBalance.java
+++ src/main/java/com/speculation1000/specdb/market/AccountBalance.java
@@ -1,6 +1,8 @@
 package com.speculation1000.specdb.market;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountBalance extends Market {
 	
@@ -19,6 +21,18 @@ public class AccountBalance extends Market {
 	
 	public BigDecimal getAmount(){
 		return amount;
+	}
+	
+	public static List<Symbol> getSymbolsListAccBalList(List<AccountBalance> accBalList){
+		List<Symbol> symbolList = new ArrayList<>();
+		for(AccountBalance ab : accBalList){
+			if(!ab.getCounter().equalsIgnoreCase("BTC")){
+				symbolList.add(new Symbol(ab.getCounter(),"BTC",ab.getExchange()));
+			}else{
+				symbolList.add(new Symbol(ab.getCounter(),"USDT",ab.getExchange()));
+			}
+		}
+		return symbolList;			
 	}
 
 }
